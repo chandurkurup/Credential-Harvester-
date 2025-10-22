9317e7b18e876b240e4c46a7f7d38091964c9a70
tester2023 r <newyeartester2023@gmail.com>
2025-10-22 06:22:20 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <FirebaseClientProvider>{children}</FirebaseClientProvider>
       </body>
     </html>
