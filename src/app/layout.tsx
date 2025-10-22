86ae02ac0b69a651c46d26bca7f220b58aff7d5e
tester2023 r <newyeartester2023@gmail.com>
2025-10-22 07:35:52 +0000

    not working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <FirebaseClientProvider>{children}</FirebaseClientProvider>
-        <Toaster />
+        <FirebaseClientProvider>
+          <main>{children}</main>
+          <Toaster />
+        </FirebaseClientProvider>
       </body>
     </html>
   );
