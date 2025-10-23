f67bc9d507a4ba0d1c873be377456329e7e7bd3c
tester2023 r <newyeartester2023@gmail.com>
2025-10-23 09:44:31 +0000

    vercel take a toomuch time to comlete sigin

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,21 +1,18 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
-import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 import firebaseConfig from './config';
 
 let app: FirebaseApp;
-let auth: Auth;
 let firestore: Firestore;
 
-// This function is for use in SERVER-SIDE code only (e.g., Genkit flows).
+// This function is for use in SERVER-SIDE code only.
 export function initializeFirebaseServer() {
   if (!getApps().length) {
     app = initializeApp(firebaseConfig);
   } else {
     app = getApp();
   }
-  auth = getAuth(app);
   firestore = getFirestore(app);
   
-  return { app, auth, firestore };
+  return { firestore };
 }
